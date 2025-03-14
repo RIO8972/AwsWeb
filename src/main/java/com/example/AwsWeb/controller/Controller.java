ee8ec267ed2bc05f51690ed2e5352f0a4d16cd74
rio <ksw89726756@gmail.com>
2025-03-15 00:16:21 +0900

    AwsWeb2

---
+++ src/main/java/com/example/AwsWeb/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.example.AwsWeb.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping("/controller")
+    public String contr(){
+        return "/test";
+    }
+}
